c89c18164481de67db69e27e6e83782521c38ac4
fhwattiheluw <fadliwattiheluw1994@gmail.com>
2023-12-27 11:24:56 +0900

    Update TabelC4Controller.php

--- laravel/app/Http/Controllers/TabelC4Controller.php
+++ laravel/app/Http/Controllers/TabelC4Controller.php
@@ -11,31 +11,7 @@ class TabelC4Controller extends Controller
     {
         //
         return view('kriteria.c4.index');
-    }
-    public function create()
-    {
-        //
-    }
-    public function store(Request $request)
-    {
-        //
-    }
-    public function show(tabelC4 $tabelC4)
-    {
-        //
-    }
-    public function edit(tabelC4 $tabelC4)
-    {
-        //
-    }
-    public function update(Request $request, tabelC4 $tabelC4)
-    {
-        //
-    }
-    public function destroy(tabelC4 $tabelC4)
-    {
-        //
-    }
+    } 
 
     // Tabel 4.1.2.2 DTPS yang Bidang Keahliannya Sesuai dengan Bidang PS
     public function tabel_4_1_2_2_index()
@@ -64,6 +40,285 @@ class TabelC4Controller extends Controller
         //
     }
     public function tabel_4_1_2_2_destroy(tabelC4 $tabelC4)
+    {
+        //
+    }
+
+    //Tabel 4.1.2.3 DTPS yang Bidang Keahliannya di Luar Bidang PS
+    public function tabel_4_1_2_3_index()
+    {
+        //
+        return view('kriteria.c4.tabel_4_1_2_3.index');
+    }
+    public function tabel_4_1_2_3_create()
+    {
+        //
+    }
+    public function tabel_4_1_2_3_store(Request $request)
+    {
+        //
+    }
+    public function tabel_4_1_2_3_show(tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_1_2_3_edit(tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_1_2_3_update(Request $request, tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_1_2_3_destroy(tabelC4 $tabelC4)
+    {
+        //
+    }
+
+    // Tabel 4.1.2.4 Rasio DTPS terhadap Mahasiswa Reguler
+    public function tabel_4_1_2_4_index()
+    {
+        //
+        return view('kriteria.c4.tabel_4_1_2_4.index');
+    }
+    public function tabel_4_1_2_4_create()
+    {
+        //
+    }
+    public function tabel_4_1_2_4_store(Request $request)
+    {
+        //
+    }
+    public function tabel_4_1_2_4_show(tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_1_2_4_edit(tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_1_2_4_update(Request $request, tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_1_2_4_destroy(tabelC4 $tabelC4)
+    {
+        //
+    }
+
+    // Tabel 4.1.2.5 Beban Kerja Dosen DTPS
+    public function tabel_4_1_2_5_index()
+    {
+        //
+        return view('kriteria.c4.tabel_4_1_2_5.index');
+    }
+    public function tabel_4_1_2_5_create()
+    {
+        //
+    }
+    public function tabel_4_1_2_5_store(Request $request)
+    {
+        //
+    }
+    public function tabel_4_1_2_5_show(tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_1_2_5_edit(tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_1_2_5_update(Request $request, tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_1_2_5_destroy(tabelC4 $tabelC4)
+    {
+        //
+    }
+
+    // Tabel 4.1.2.6 Kegiatan Mengajar Dosen Tetap - Semester Gasal & Semester Genap
+    public function tabel_4_1_2_6_index()
+    {
+        //
+        return view('kriteria.c4.tabel_4_1_2_6.index');
+    }
+    public function tabel_4_1_2_6_create()
+    {
+        //
+    }
+    public function tabel_4_1_2_6_store(Request $request)
+    {
+        //
+    }
+    public function tabel_4_1_2_6_show(tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_1_2_6_edit(tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_1_2_6_update(Request $request, tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_1_2_6_destroy(tabelC4 $tabelC4)
+    {
+        //
+    }
+
+    // Tabel 4.1.2.7 Jumlah Bimbingan Tugas Akhir atau Skripsi, Tesis, dan Disertasi
+    public function tabel_4_1_2_7_index()
+    {
+        //
+        return view('kriteria.c4.tabel_4_1_2_7.index');
+    }
+    public function tabel_4_1_2_7_create()
+    {
+        //
+    }
+    public function tabel_4_1_2_7_store(Request $request)
+    {
+        //
+    }
+    public function tabel_4_1_2_7_show(tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_1_2_7_edit(tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_1_2_7_update(Request $request, tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_1_2_7_destroy(tabelC4 $tabelC4)
+    {
+        //
+    }
+
+    // Tabel 4.1.2.8 Prestasi DTPS
+    public function tabel_4_1_2_8_index()
+    {
+        //
+        return view('kriteria.c4.tabel_4_1_2_8.index');
+    }
+    public function tabel_4_1_2_8_create()
+    {
+        //
+    }
+    public function tabel_4_1_2_8_store(Request $request)
+    {
+        //
+    }
+    public function tabel_4_1_2_8_show(tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_1_2_8_edit(tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_1_2_8_update(Request $request, tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_1_2_8_destroy(tabelC4 $tabelC4)
+    {
+        //
+    }
+
+    // Tabel 4.1.2.9 Pengembangan Kompetensi DTPS
+    public function tabel_4_1_2_9_index()
+    {
+        //
+        return view('kriteria.c4.tabel_4_1_2_9.index');
+    }
+    public function tabel_4_1_2_9_create()
+    {
+        //
+    }
+    public function tabel_4_1_2_9_store(Request $equest)
+    {
+        //
+    }
+    public function tabel_4_1_2_9_show(tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_1_2_9_edit(tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_1_2_9_update(Request $request, tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_1_2_9_destroy(tabelC4 $tabelC4)
+    {
+        //
+    }
+
+    // Tabel 4.2.2.2 Profil Tendik
+    public function tabel_4_2_2_2_index()
+    {
+        //
+        return view('kriteria.c4.tabel_4_2_2_2.index');
+    }
+    public function tabel_4_2_2_2_create()
+    {
+        //
+    }
+    public function tabel_4_2_2_2_store(Request $equest)
+    {
+        //
+    }
+    public function tabel_4_2_2_2_show(tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_2_2_2_edit(tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_2_2_2_update(Request $request, tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_2_2_2_destroy(tabelC4 $tabelC4)
+    {
+        //
+    }
+
+    // Tabel 4.2.2.3 Pengembangan Kompetensi dan Karier Tendik
+    public function tabel_4_2_2_3_index()
+    {
+        //
+        return view('kriteria.c4.tabel_4_2_2_3.index');
+    }
+    public function tabel_4_2_2_3_create()
+    {
+        //
+    }
+    public function tabel_4_2_2_3_store(Request $equest)
+    {
+        //
+    }
+    public function tabel_4_2_2_3_show(tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_2_2_3_edit(tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_2_2_3_update(Request $request, tabelC4 $tabelC4)
+    {
+        //
+    }
+    public function tabel_4_2_2_3_destroy(tabelC4 $tabelC4)
     {
         //
     }
