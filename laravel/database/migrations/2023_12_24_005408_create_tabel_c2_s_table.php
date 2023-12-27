80bb5921702b35943a12cf8467f11638e6dea4c3
fhwattiheluw <fadliwattiheluw1994@gmail.com>
2023-12-27 23:19:06 +0900

    Update 2023_12_24_005408_create_tabel_c2_s_table.php

--- laravel/database/migrations/2023_12_24_005408_create_tabel_c2_s_table.php
+++ laravel/database/migrations/2023_12_24_005408_create_tabel_c2_s_table.php
@@ -6,26 +6,51 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('tabel_c2_s', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+  /**
+  * Run the migrations.
+  *
+  * @return void
+  */
+  public function up()
+  {
+    //create : Tabel 2.2.2 Data Kerja Sama - Bidang Pendidikan
+    Schema::create('tabel_k2_bidang_pendidikan', function (Blueprint $table) {
+      $table->id();
+      $table->timestamps();
+    });
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('tabel_c2_s');
-    }
+    //create : Tabel 2.2.2 Data Kerja Sama - Bidang Penelitian
+    Schema::create('tabel_k2_bidang_penelitian', function (Blueprint $table) {
+      $table->id();
+      $table->timestamps();
+    });
+
+    //create : Tabel 2.2.2 Data Kerja Sama - Bidang Pengabdian kepada Masyarakat (PkM)
+    Schema::create('tabel_k2_bidang_pkm', function (Blueprint $table) {
+      $table->id();
+      $table->timestamps();
+    });
+
+    //create : Tabel 2.2.2 Data Kerja Sama - Bidang Pengembangan Kelembagaan: SDM, Sarana/Prasarana, Publikasi, HKI, Paten, Teknologi Pembelajaran, dll.
+    Schema::create('tabel_k2_bidang_pengembangan_kelembagaan', function (Blueprint $table) {
+      $table->id();
+      $table->timestamps();
+    });
+
+    
+  }
+
+  /**
+  * Reverse the migrations.
+  *
+  * @return void
+  */
+  public function down()
+  {
+
+    Schema::dropIfExists('tabel_k2_bidang_pendidikan');
+    Schema::dropIfExists('tabel_k2_bidang_penelitian');
+    Schema::dropIfExists('tabel_k2_bidang_pkm');
+    Schema::dropIfExists('tabel_k2_bidang_pengembangan_kelembagaan');
+  }
 };
